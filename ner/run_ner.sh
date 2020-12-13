7a27f0a7093055c86e6e74fa56de927ef813deab
uqarahi2 <uqarahi2@wiener.hpc.dc.uq.edu.au>
2020-12-13 22:52:11 +1000

    code and script to run ner experiments

---
+++ ner/run_ner.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#conda create --name indolem
+#conda activate indolem
+#conda install -c pytorch pytorch
+pip install seqeval
+pip install transformers==2.9.0
+
+export MAX_LENGTH=128
+#export BERT_MODEL=bert-base-multilingual-cased
+export BERT_MODEL=/scratch/itee/ash/indobert/Indonesian/indobert/
+export BERT_MODEL="indolem/indobert-base-uncased"
+
+for f in 1 2 3 4 5
+do
+
+export FOLD=0$f
+export OUTPUT_DIR=indoner$FOLD
+export BATCH_SIZE=32
+export NUM_EPOCHS=30
+export SAVE_STEPS=750
+export SEED=1
+export DATA_DIR=./data/nerugm
+
+
+cat $DATA_DIR/train.$FOLD.tsv | tr '\t' ' '  | tr '  ' ' ' > train.txt
+cat $DATA_DIR/dev.$FOLD.tsv  | tr '\t' ' '  | tr '  ' ' ' > dev.txt
+cat $DATA_DIR/test.$FOLD.tsv  | tr '\t' ' '  | tr '  ' ' ' > test.txt
+cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > labels.txt
+
+
+python3 run_ner.py \
+--data_dir . \
+--labels ./labels.txt \
+--model_name_or_path $BERT_MODEL \
+--output_dir $OUTPUT_DIR \
+--max_seq_length  $MAX_LENGTH \
+--num_train_epochs $NUM_EPOCHS \
+--per_gpu_train_batch_size $BATCH_SIZE \
+--save_steps $SAVE_STEPS \
+--seed $SEED \
+--do_train \
+--do_eval \
+--do_predict
+
+done
